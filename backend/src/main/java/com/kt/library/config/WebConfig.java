fc2d431791581ac8d0f416745f47a4a204e00ae9
최준형 <hjjh5019@naver.com>
2025-12-05 15:57:22 +0900

    Check connect

---
+++ backend/src/main/java/com/kt/library/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.kt.library.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 API 경로 허용
+                .allowedOrigins("http://localhost:5173") // 프론트엔드 도메인
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
