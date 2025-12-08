71b67c61879cba2f07a6735905c53715093926b7
seohyun <yseohyun11@gmail.com>
2025-12-08 11:23:55 +0900

    LoginRequest추가, UserServiceImpl 아이디로 회원 조회와 비밀번호 검증 추가

--- backend/src/main/java/com/kt/library/config/WebConfig.java
+++ backend/src/main/java/com/kt/library/config/WebConfig.java
@@ -1,6 +1,9 @@
 package com.kt.library.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,5 +17,9 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
