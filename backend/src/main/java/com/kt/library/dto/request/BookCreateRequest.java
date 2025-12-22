b69f430cb0ba5e36940275234e7b9df9c8936a6b
geunwoo <garroid@nver.com>
2025-12-22 13:47:30 +0900

    수정: 필드 추가

--- backend/src/main/java/com/kt/library/dto/request/BookCreateRequest.java
+++ backend/src/main/java/com/kt/library/dto/request/BookCreateRequest.java
@@ -1,3 +1,21 @@
+//package com.kt.library.dto.request;
+//
+//import com.kt.library.domain.Book;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Getter
+//@Setter
+//public class BookCreateRequest {
+//
+//    private String title;
+//    private String content;
+//    private String author;
+//    private Book.Language language;
+//    private Book.Genre genre;
+//}
+
+
 package com.kt.library.dto.request;
 
 import com.kt.library.domain.Book;
@@ -13,4 +31,8 @@ public class BookCreateRequest {
     private String author;
     private Book.Language language;
     private Book.Genre genre;
+
+    // ★ 이 한 줄이 없어서 에러가 났던 겁니다!
+    // 이제 프론트엔드에서 보낸 apiKey를 여기서 받아서 서비스로 넘겨줍니다.
+    private String apiKey;
 }
